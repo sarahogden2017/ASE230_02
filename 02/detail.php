d43efe9020458ddf11e88503a9a7cb83ac229896
Sarah Ogden <sarahogden2017@gmail.com>
2024-09-06 14:34:11 -0400

    edited details.php

--- 02/detail.php
+++ 02/detail.php
@@ -7,7 +7,13 @@ $team=[
 				'name'=>'Sarah Ogden',
 				'role'=>'team member',
 				'image'=>'assets/images/sarah.jpg',
-
+				'bio'=>'Sarah is a team member who is passionate about coding and design. She is a quick learner and a great team player.',
+				'job'=>'Student IT Support Specialist',
+				'email'=>'ogdens2@nku.edu',
+				'phone'=>'614-593-5929',
+				'linkedin'=>'linkedin.com/in/ogden100',
+				'github'=>'github.com/sarahogden2017',
+				'website'=>'sarahogden2017.github.io',
 		],
 ]
 ?>
@@ -49,18 +55,18 @@ $team=[
 				    <div class="col">
 					    <div class="row p-4 justify-content-center justify-content-md-between">
 						    <div class="primary-info col-auto">
-							    <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase">Your name</h1>
-							    <div class="title mb-3">Your desired job title</div>
+							  <h1 class="name mt-0 mb-1 text-white text-uppercase text-uppercase"><?= $team[$i]['name'] ?></h1>
+							    <div class="title mb-3"><?= $team[$i]['job']?></div>
 							    <ul class="list-unstyled">
-								    <li class="mb-2"><a class="text-link" href="#"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i>your@email.com</a></li>
-								    <li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i>0123 456 78900</a></li>
+								  <li class="mb-2"><a class="text-link" href="#"><i class="far fa-envelope fa-fw me-2" data-fa-transform="grow-3"></i><?= $team[$i]['email'] ?></a></li>
+								  <li><a class="text-link" href="#"><i class="fas fa-mobile-alt fa-fw me-2" data-fa-transform="grow-6"></i><?= $team[$i]['github'] ?></a></li>
 							    </ul>
 						    </div><!--//primary-info-->
 						    <div class="secondary-info col-auto mt-2">
 							    <ul class="resume-social list-unstyled">
-					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span>linkedin.com/in/yourlink</a></li>
-					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span>github.com/yourhandle</a></li>
-					                <li><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fas fa-globe"></i></span>yourwebsite.com</a></li>
+					        <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-linkedin-in fa-fw"></i></span><?= $team[$i]['linkedin'] ?></a></li>
+					                <li class="mb-3"><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fab fa-github-alt fa-fw"></i></span><?= $team[$i]['github'] ?></a></li>
+					                <li><a class="text-link" href="#"><span class="fa-container text-center me-2"><i class="fas fa-globe"></i></span><?= $team[$i]['website'] ?></a></li>
 							    </ul>
 						    </div><!--//secondary-info-->
 					    </div><!--//row-->
